c07847bad77490492f315eab54f2e0a70cf01675
David Zhou <wi21b026@technikum-wien.at>
2022-05-19 12:29:07 +0200

    add sum function java doc

--- src/main/java/com/example/restcalculator/CalculatorController.java
+++ src/main/java/com/example/restcalculator/CalculatorController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalculatorController {
 
+    /**
+     * Does sum calculation
+     * @param a int
+     * @param b int
+     * @return sum int
+     */
     @RequestMapping("/sum")
     public int sum(@RequestParam int a, @RequestParam int b){
         return a + b;
