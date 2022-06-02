ce18ab5c0f48854f0f361aab01180d33d344bbc8
David Zhou <wi21b026@technikum-wien.at>
2022-06-02 16:20:01 +0200

    add multiple function java doc

--- src/main/java/com/example/restcalculator/CalculatorController.java
+++ src/main/java/com/example/restcalculator/CalculatorController.java
@@ -19,8 +19,32 @@ public class CalculatorController {
     }
 
     @RequestMapping("/sqr")
-    public int sum(@RequestParam int a){
+    public int sqr(@RequestParam int a){
         return a * a;
     }
 
+    @RequestMapping("/multiply")
+    public int multiply(@RequestParam int a, @RequestParam int b){
+        return a * b;
+    }
+
+    @RequestMapping("/division")
+    public double division(@RequestParam int a, @RequestParam int b){
+        return (double)a / (double)b;
+    }
+
+    @RequestMapping("/abs")
+    public int abs(@RequestParam int a){
+        return Math.abs(a);
+    }
+
+    @RequestMapping("/pow")
+    public double expo(@RequestParam int a, @RequestParam int b){
+        return Math.pow(a, b);
+    }
+
+    @RequestMapping("/min")
+    public int min(@RequestParam int a, @RequestParam int b){
+        return Math.min(a, b);
+    }
 }
